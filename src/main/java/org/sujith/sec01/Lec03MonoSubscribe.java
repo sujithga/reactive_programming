c1bddc3f47ef8ec54546364a3321cb79d6820e7d
sujithga <sujith.ga@gmail.com>
2024-06-15 14:15:09 +0530

    Reactive programming initial commit

---
+++ src/main/java/org/sujith/sec01/Lec03MonoSubscribe.java
@@ -0,0 +1,12 @@
+package org.sujith.sec01;
+
+import reactor.core.publisher.Mono;
+
+public class Lec03MonoSubscribe {
+
+    public static void main(String[] args){
+        Mono<Integer> mono = Mono.just(2);
+        mono.subscribe(c -> System.out.printf("Received  %d",c));
+
+    }
+}
