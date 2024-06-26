d13237017bc91ebce0ee36a3be3bede554ccd326
sujithga <sujith.ga@gmail.com>
2024-06-26 17:48:36 +0530

    Reactive programming new files

--- src/main/java/org/sujith/sec01/Lec03MonoSubscribe.java
+++ src/main/java/org/sujith/sec01/Lec03MonoSubscribe.java
@@ -2,6 +2,8 @@ package org.sujith.sec01;
 
 import reactor.core.publisher.Mono;
 
+import java.util.function.Supplier;
+
 public class Lec03MonoSubscribe {
 
     public static void main(String[] args){
@@ -9,5 +11,16 @@ public class Lec03MonoSubscribe {
         mono.subscribe(c -> System.out.printf("Received  %d",c));
         System.out.println("Hello");
 
+
+        Mono<String> mono2 = Mono.fromSupplier(() -> { return "hi";});
+        mono2.subscribe(c -> System.out.printf("Received  %s",c));
+
+
+        Supplier supplier;
+
+    }
+
+    static String getStr(){
+        return "hi";
     }
 }
